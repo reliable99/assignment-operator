ed8f9015f7fcdc378a636382c7e81d2e2ce28b7b
reliable99 <abdulmuhminsodiq@gmail.com>
2023-10-12 11:10:21 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+console.log("assignment operator");
+
+let number = 5;
+
+number += 5; // the same as number = number + 5;
+// number -= 5; // the same as number = number - 5;
+// number *= 5; // the same as number = number * 5;
+// number /= 5; // the same as number = number / 5;
+
+console.log(number);
+
+// An assignment operator assigns a value to its left operand based on the value of its right operand.
+
+//The addition assignment can also be used with strings! Let me show you:
+
+let string = "hello,";
+string += "abdulmuhmin";
+console.log(string);
